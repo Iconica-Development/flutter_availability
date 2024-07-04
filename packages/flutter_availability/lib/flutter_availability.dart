48bfc2cdb26511a2c8b543ee00a96dbb4313b604
Freek van de Ven <freek@freekvandeven.nl>
2024-07-04 09:37:58 +0200

    feat: add flutter_availability configuration

--- packages/flutter_availability/lib/flutter_availability.dart
+++ packages/flutter_availability/lib/flutter_availability.dart
@@ -1,8 +1,8 @@
 ///
 library flutter_availability;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export "src/config/availability_options.dart";
+export "src/config/availability_translations.dart";
+export "src/screens/availability_overview.dart";
+export "src/userstory/navigator_userstory.dart";
+export "src/userstory/userstory_configuration.dart";
