73048837a360ff18379d95a129d95524172f8587
Freek van de Ven <freek@freekvandeven.nl>
2024-07-05 13:59:40 +0200

    feat: add device preview package to the example app
    With Device Preview it is easy to view the userstory on different kinds of devices and change settings like language

--- apps/example/lib/main.dart
+++ apps/example/lib/main.dart
@@ -1,8 +1,19 @@
+import "package:device_preview/device_preview.dart";
 import "package:flutter/material.dart";
 import "package:flutter_availability/flutter_availability.dart";
 
 void main() {
-  runApp(const App());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      isToolbarVisible: true,
+      availableLocales: const [
+        Locale("en_US"),
+        Locale("nl_NL"),
+      ],
+      builder: (_) => const App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -10,6 +21,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
+        builder: DevicePreview.appBuilder,
+        locale: DevicePreview.locale(context),
+        supportedLocales: const [
+          Locale("en", "US"),
+          Locale("nl", "NL"),
+        ],
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
         home: AvailabilityUserStory(
           userId: "",
           options: AvailabilityOptions(),
