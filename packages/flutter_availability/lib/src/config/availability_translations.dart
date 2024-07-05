bb01592f54d7b7af8ffa6f1f63221ba4ef8b68c9
Freek van de Ven <freek@freekvandeven.nl>
2024-07-05 16:09:16 +0200

    feat: add calendar view without interaction

--- packages/flutter_availability/lib/src/config/availability_translations.dart
+++ packages/flutter_availability/lib/src/config/availability_translations.dart
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+import "package:flutter/material.dart";
+
 /// Class that holds all translatable strings for the availability userstory
 class AvailabilityTranslations {
   /// AvailabilityTranslations constructor where everything is required.
@@ -14,6 +16,8 @@ class AvailabilityTranslations {
     required this.templateLegendTitle,
     required this.overviewScreenTitle,
     required this.createTemplateButton,
+    required this.monthYearFormatter,
+    required this.weekDayAbbreviatedFormatter,
   });
 
   /// AvailabilityTranslations constructor where everything is optional.
@@ -24,6 +28,8 @@ class AvailabilityTranslations {
     this.templateLegendTitle = "Templates",
     this.createTemplateButton = "Create a new template",
     this.overviewScreenTitle = "Availability",
+    this.monthYearFormatter = _defaultMonthYearFormatter,
+    this.weekDayAbbreviatedFormatter = _defaultWeekDayAbbreviatedFormatter,
   });
 
   /// The title shown above the calendar
@@ -40,4 +46,51 @@ class AvailabilityTranslations {
 
   /// The label on the button to go to the tempalte creation page
   final String createTemplateButton;
+
+  /// Gets the month and year formatted as a string
+  ///
+  /// The default implementation is `MonthName Year` in english
+  final String Function(BuildContext, DateTime) monthYearFormatter;
+
+  /// Gets the abbreviated name of a weekday
+  ///
+  /// The default implementation is the first 2 letters of
+  /// the weekday in english
+  final String Function(BuildContext, DateTime) weekDayAbbreviatedFormatter;
 }
+
+String _defaultWeekDayAbbreviatedFormatter(
+  BuildContext context,
+  DateTime date,
+) =>
+    _getWeekDayAbbreviation(date.weekday);
+
+String _defaultMonthYearFormatter(BuildContext context, DateTime date) =>
+    "${_getMonthName(date.month)} ${date.year}";
+
+String _getWeekDayAbbreviation(int weekday) => switch (weekday) {
+      1 => "Mo",
+      2 => "Tu",
+      3 => "We",
+      4 => "Th",
+      5 => "Fr",
+      6 => "Sa",
+      7 => "Su",
+      _ => "",
+    };
+
+String _getMonthName(int month) => switch (month) {
+      1 => "January",
+      2 => "February",
+      3 => "March",
+      4 => "April",
+      5 => "May",
+      6 => "June",
+      7 => "July",
+      8 => "August",
+      9 => "September",
+      10 => "October",
+      11 => "November",
+      12 => "December",
+      _ => "",
+    };
