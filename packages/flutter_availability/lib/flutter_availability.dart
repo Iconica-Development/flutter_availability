70f8ac1db06bd38d4fdde805a8bf86d39c300bc6
Freek van de Ven <freek@freekvandeven.nl>
2024-07-17 13:29:09 +0200

    fix: export the flutter_availability_data_interface from the userstory
    This makes it easier to use the userstory when you need to create your own datainterface implementation

--- packages/flutter_availability/lib/flutter_availability.dart
+++ packages/flutter_availability/lib/flutter_availability.dart
@@ -1,6 +1,8 @@
 ///
 library flutter_availability;
 
+export "package:flutter_availability_data_interface/flutter_availability_data_interface.dart";
+
 export "src/config/availability_options.dart";
 export "src/config/availability_translations.dart";
 export "src/ui/screens/availability_overview.dart";
