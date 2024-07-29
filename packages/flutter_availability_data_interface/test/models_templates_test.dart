a819a4df08f5a9fc8eb347902e172f3e0827d1a4
Joey Boerwinkel <joey@iconica.nl>
2024-07-29 09:54:30 +0200

    chore: remove flutter dependency from data interface

--- packages/flutter_availability_data_interface/test/models_templates_test.dart
+++ packages/flutter_availability_data_interface/test/models_templates_test.dart
@@ -1,5 +1,5 @@
 import "package:flutter_availability_data_interface/flutter_availability_data_interface.dart";
-import "package:flutter_test/flutter_test.dart";
+import "package:test/test.dart";
 
 void main() {
   group("AvailabilityTemplate", () {
