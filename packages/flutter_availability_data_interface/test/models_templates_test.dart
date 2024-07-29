860fae0ffc491177da61cac12f68c06fb9e63a96
Freek van de Ven <35540954+freekvandeven@users.noreply.github.com>
2024-07-29 10:38:45 +0200

    Revert "chore: remove flutter dependency from data interface"
    This reverts commit 3924a5804c97141aca7dc4b37878fa1ae520d2c8.

--- packages/flutter_availability_data_interface/test/models_templates_test.dart
+++ packages/flutter_availability_data_interface/test/models_templates_test.dart
@@ -1,5 +1,5 @@
 import "package:flutter_availability_data_interface/flutter_availability_data_interface.dart";
-import "package:test/test.dart";
+import "package:flutter_test/flutter_test.dart";
 
 void main() {
   group("AvailabilityTemplate", () {
